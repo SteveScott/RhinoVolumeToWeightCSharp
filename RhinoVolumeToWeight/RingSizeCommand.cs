0378e91041f9d2eb0ef5e7e2cb7a332ca0e166e0
Steve Scott <stevescott517@gmail.com>
2025-01-06 17:28:30 -0500

    ring size command template.

---
+++ RhinoVolumeToWeight/RingSizeCommand.cs
@@ -0,0 +1,31 @@
+﻿using Rhino;
+using Rhino.Commands;
+using System;
+
+
+
+namespace RhinoVolumeToWeight
+{
+    public class RingSize : Command
+    {
+        //if you want the value metalIndex to persist, you need to define it at the class level here, not inside the RunCommand method.
+        private double ringSize = 2;
+        public RingSize()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static RingSize Instance { get; private set; }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName => "RingSize";
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            return Rhino.Commands.Result.Failure;
+        }
+    }
+}
